81cfc6562cd92e1def4b2e841d48674b4e81c805
abbyssoul <abbyssoul@gmail.com>
2019-07-11 20:02:17 +1000

    Added basic test skeleton

---
+++ test/test_protocol.cpp
@@ -0,0 +1,31 @@
+/*
+*  Copyright 2019 Ivan Ryabov
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*/
+/*******************************************************************************
+ * libTribe Unit Test Suit
+ *	@file test/test_model.cpp
+ *	@brief		Test suit for tribe::model
+ ******************************************************************************/
+#include "tribe/protocol.hpp"    // Class being tested.
+
+#include <gtest/gtest.h>
+
+
+using namespace tribe;
+using namespace Solace;
+
+
+TEST(TestProtocol, test_1) {
+}
